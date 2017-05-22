076eeffdc38aea8f999afc632d5290d6e274c688
beom\hansb <aronix@naver.com>
2017-05-23 01:18:09 +0900

    Monster 클래스 구현

--- 미니포켓몬고/미니포켓몬고/Monster.cpp
+++ 미니포켓몬고/미니포켓몬고/Monster.cpp
@@ -1,11 +1,21 @@
 #include "Monster.h"
 
 
-
-Monster::Monster()
-{
+Monster::Monster(
+	int id,
+	string nickname,
+	int hp, 
+	int attk,
+	int def,
+	int cp)
+	{
+	this->id = id;
+	this->nickname = nickname;
+	this->hp = hp;
+	this->attk = attk;
+	this->def = def;
+	this->cp = cp;
 }
-
 
 Monster::~Monster()
 {
--- 미니포켓몬고/미니포켓몬고/Monster.h
+++ 미니포켓몬고/미니포켓몬고/Monster.h
@@ -1,21 +1,23 @@
 #pragma once
 #include <iostream>
 #include <string>
+
 using namespace std;
 class Monster
 {
-	int num;		//몬스터의 ID
-	string name;	 //몬스터 이름
-	int type;		 //타입
-	int probability; //확률
-
+	int id;			 //몬스터 id
 	string nickname; //몬스터의 별명
 	int hp;			 //몬스터 체력
-	int atk;		 //몬스터 공격력
+	int attk;		 //몬스터 공격력
 	int def;		 //몬스터 방어력
 	int cp;			 //몬스터의 cp
 public:
-	Monster();
+	Monster(int id,
+		string nickname,
+		int hp,
+		int attk,
+		int def,
+		int cp);
 	~Monster();
 };
 
