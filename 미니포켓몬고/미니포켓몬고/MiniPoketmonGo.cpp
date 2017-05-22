ceb7919ec182629ffe59e86452a2fbe5f3dba6cd
beom\hansb <aronix@naver.com>
2017-05-23 01:20:05 +0900

    MiniPoketmonGo 클래스 구현

--- 미니포켓몬고/미니포켓몬고/MiniPoketmonGo.cpp
+++ 미니포켓몬고/미니포켓몬고/MiniPoketmonGo.cpp
@@ -1,12 +1,28 @@
 #include "MiniPoketmonGo.h"
-
-
+#include "FileManager.h"
+#include "Player.h"
+#include <fstream>
 
 MiniPoketmonGo::MiniPoketmonGo()
 {
+	fm = new FileManager();
+	loadData();
 }
 
 
 MiniPoketmonGo::~MiniPoketmonGo()
 {
+	
+}
+void MiniPoketmonGo::loadData(){
+	monsters = fm->readMonsterFromFile();
+	balls = fm->readMonsterBallFromFile();
+	player = fm->readPlayerFromFile(monsters, balls);
+}
+
+
+void MiniPoketmonGo::debug(){
+	for (int i = 0; i < monsters.size(); i++){
+		cout << monsters.at(i).getName() << endl;
+	}
 }
--- 미니포켓몬고/미니포켓몬고/MiniPoketmonGo.h
+++ 미니포켓몬고/미니포켓몬고/MiniPoketmonGo.h
@@ -1,30 +1,32 @@
 #pragma once
-#include "player.h"
+#include "MonsterData.h"
 #include "Ball.h"
-
+#include "FileManager.h"
 
 class MiniPoketmonGo
 {
-	Player player;		//게임 플레이어
-	Monster* monsters;	//몬스터 데이터
-	Ball balls;			//몬스터볼 데이터
+	FileManager* fm;		//파일 관리자
+	Player* player;		//게임 플레이어
+	vector<MonsterData> monsters;	//몬스터 데이터
+	vector<Ball> balls;		//몬스터볼 데이터
 
 public:
 	MiniPoketmonGo();
 	~MiniPoketmonGo();
-	void loadData();		
-	void loadPlayerData();	//플레이어 파일데이터 로드
-	void loadMosterData();	//몬스터 파일데이터 로드
-	void loadBallData();	//몬스터볼 파일데이터 로드
+	void loadData();	
+	/*
 	void saveData();
 	void savePlayerData();  //플레이어 파일데이터 세이브
 	void saveMonsterData(); //몬스터 파일데이터 세이브
 	void saveBallData();	//몬스터볼 파일데이터 세이브
 
+	void menu_main();
 	void menuPlayer();
 	void menuMonster();
 	void menuBuy();
 	void menuGetPoketmon();
 	void menuExit();
+	*/
+	void debug();
 };
 
